b60a130ac621cceae82efb7d858bfac37481c1eb
Bruno Amaral Ianeczek <87914395+BrunoIaneczek@users.noreply.github.com>
2021-12-10 20:46:58 -0300

    atualizando

---
+++ Aplicações_com_estrutura_de_repetição/contagem_de_pares/contagem_de_pares.js
@@ -0,0 +1,18 @@
+function contar(){
+    let numeroinicial = document.getElementById('ninicial')
+    let numerofinal = document.getElementById('nfinal')
+    let res = document.getElementById('res')
+
+    if (numeroinicial.value.length == 0 || numerofinal.value.length == 0){
+        alert('ERRO! digite os valores solicitados')
+    }else{
+        res.innerHTML = `intervalo<br>`
+        let ni = Number(numeroinicial.value)
+        let nf = Number(numerofinal.value)
+        for(let c = ni; c <= nf; c++){
+            if (c%2 == 0){
+                res.innerHTML += ` ${c}<br>`
+            }
+        }
+    }
+}
